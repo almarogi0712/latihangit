3303e6e412f9ef375698b97217d6d59e02810fc3
githa <gita.refina1@gmail.com>
2021-12-15 15:12:51 +0700

    tambah file githa

---
+++ githa/jquery.js
@@ -0,0 +1,19 @@
+
+// $(document).ready(() =>{
+//     $.ajax({
+//         method: "GET", //HTTP method cth: “POST”, “GET”, “PUT”
+//         url: "https://random.dog/woof.json", //Url request nya
+//        })
+//         .done(function() {
+//             console.log(data)
+
+//         })
+//         .fail(function() {
+//           alert( "error" );
+//         })
+//         .always(function() {
+//           alert( "complete" );
+//         });
+       
+//     });
+    
